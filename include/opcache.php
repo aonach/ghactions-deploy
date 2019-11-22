3cad320e4774b27a854edff6b8908828ec51c69e
Evgeny Levinsky <el@ideal-code.ru>
2019-11-22 19:00:21 +0300

    Added description about paths to php sockets

--- include/opcache.php
+++ include/opcache.php
@@ -18,6 +18,8 @@ desc('PHP Opcache flush');
 task('php:opcache:flush', function() {
     $cacheToolVersion = version_compare(get('php_version'), '7.2', '>=') ? '' : '-3.2.1';
 
+    // Php socket to clear opcache can be located in different places
+    // on different servers, just add your paths, if needed
     run('
     {{bin/curl}} -s http://gordalina.github.io/cachetool/downloads/cachetool'.$cacheToolVersion.'.phar -o cachetool.phar
     chmod +x cachetool.phar
