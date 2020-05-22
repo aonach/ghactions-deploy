365f9ab46ea5dcf7619595d0b9e9769b3ac60b97
Evgeny Levinsky <evgeny@aonach.com>
2020-05-22 13:26:55 +0300

    Move out php sock paths to separate variable

--- include/opcache.php
+++ include/opcache.php
@@ -2,6 +2,11 @@
 namespace Deployer;
 
 /**
+ * Configuration
+ */
+set('php_sock_path', '~/run/*.php-fpm.sock,/var/run/$(whoami)-remi-safe-php*.sock');
+
+/**
  * Tasks
  */
 desc('PHP Opcache flush');
@@ -15,7 +20,7 @@ task('php:opcache:flush', function() {
     fi');
 
     run('
-    for sock in {~/run/*.php-fpm.sock,/var/run/$(whoami)-remi-safe-php*.sock}; do
+    for sock in {{{php_sock_path}}}; do
         if [ -S $sock ]; then
             ~/cachetool/bin/cachetool opcache:reset --fcgi=$sock && \
             echo "Opcache was cleared (php sock is $sock)"
