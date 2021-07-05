abc8c38c1bf641522488526fac6775e40f904b16
Joseph Smyth <joe@aonach.com>
2021-07-05 18:39:35 +0100

    TASK-23275393 testing lock environment variable

--- include/lock_env.php
+++ include/lock_env.php
@@ -1,6 +1,8 @@
 <?php
 namespace Deployer;
 
+
+set( 'lock_env', get( 'lock_env' ) );
 /**
  * Tasks
  */
@@ -8,8 +10,7 @@ desc('Lock environment with htaccess');
 task('deploy:lock_env', function() {
   //$currentExists = test('[ -d {{deploy_path}}/current ]');
   //&& test("[ -f {{release_path}}/$file ]"
-
-  $x = get( 'lock_env' );
-  echo $x;
+  run ( 'echo "Lock env section"' );
+  run( 'echo {{{lock_env}}}' );
 
 })->setPrivate();
