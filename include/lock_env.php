25b44fd286eb65529e5ab19a04900df4a1875094
Joseph Smyth <joe@aonach.com>
2021-07-05 21:33:41 +0100

    Trying to force version

--- include/lock_env.php
+++ include/lock_env.php
@@ -11,7 +11,7 @@ task('deploy:lock_env', function() {
   //$currentExists = test('[ -d {{deploy_path}}/current ]');
   //&& test("[ -f {{release_path}}/$file ]"
   run ( 'echo "Lock env section"' );
-  run( 'echo {{{lock_env}}}' );
+  run( 'echo {{lock_env}}' );
 
   $lock_env=get( 'lock_env' );
 
